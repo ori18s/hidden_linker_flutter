62115d3bb128c055870b77ea6ed4f2754ee5bc8d
duck90 <57226477+duck90@users.noreply.github.com>
2021-08-25 17:26:52 +0900

    hiddenLinker_210825

--- lib/main.dart
+++ lib/main.dart
@@ -1,120 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:hidden_linker_flutter/screens/home.dart';
+import 'package:hidden_linker_flutter/screens/first.dart';
 
-class Link {
-  String linkId;
-  String name;
-  String sequence;
-  String url;
-
-  Link({this.name, this.linkId, this.url, this.sequence});
-}
-
-class TopTenList extends StatefulWidget {
-  @override
-  _TopTenListState createState() => _TopTenListState();
-}
-
-class _TopTenListState extends State<TopTenList> {
-  List<Link> _items = [
-    Link(
-        linkId: "1", name: "구글", url: "https://www.google.com/", sequence: "1"),
-    Link(
-        linkId: "2", name: "네이버", url: "https://www.naver.com/", sequence: "2"),
-    Link(
-        linkId: "3",
-        name: "페이스북",
-        url: "https://www.facebook.com/",
-        sequence: "3"),
-    Link(
-        linkId: "4", name: "카카오", url: "https://www.kakao.com/", sequence: "4"),
-    Link(
-        linkId: "5",
-        name: "고대디",
-        url: "https://kr.godaddy.com/",
-        sequence: "5"),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Hidden Linker"),
-      ),
-      body: ReorderableListView(
-        onReorder: onReorder,
-        children: _getListItems(),
-      ),
-    );
-  }
-
-  void onReorder(int oldIndex, int newIndex) {
-    if (newIndex > oldIndex) {
-      newIndex -= 1;
-    }
-
-    setState(() {
-      Link link = _items[oldIndex];
-
-      _items.removeAt(oldIndex);
-      _items.insert(newIndex, link);
-    });
-  }
-
-  List<Widget> _getListItems() => _items
-      .asMap()
-      .map((i, item) => MapEntry(i, _buildTenableListTile(item, i)))
-      .values
-      .toList();
-
-  Widget _buildTenableListTile(Link item, int index) {
-    return Dismissible(
-      key: Key(item.linkId),
-      dismissThresholds: {
-        DismissDirection.endToStart: 0.4,
-        DismissDirection.startToEnd: 0.4,
-      },
-      crossAxisEndOffset: 0.1,
-      onDismissed: (direction) {
-        setState(() {
-          _items.removeAt(index);
-        });
-      },
-      background: Container(color: Colors.red),
-      child: ListTile(
-        key: ValueKey(item.linkId),
-        title: Text(
-          'ID: ${item.linkId} / seq: ${item.sequence} / name: ${item.name}',
-          style: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        subtitle: Text(
-          'url: ${item.url}',
-          style: TextStyle(
-            color: Colors.black,
-          ),
-        ),
-        onTap: () {},
-      ),
-    );
-  }
-}
-
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Hidden Linker',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: TopTenList(),
+      home: MyHomePage(title: 'Hidden Linker'),
     );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _children = [Home(), First()];
+  void _onTap(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        onTap: _onTap,
+        currentIndex: _currentIndex,
+        items: [
+          new BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          new BottomNavigationBarItem(
+            icon: Icon(Icons.mail),
+            title: Text('First'),
+          ),
+        ]));
   }
 }
---
+++ lib/screens/home.dart
@@ -0,0 +1,214 @@
+ import 'package:flutter/material.dart';
+ import 'package:flutter/foundation.dart';
+ import 'package:intl/intl.dart';
+ import 'package:metadata_fetch/metadata_fetch.dart';
+ import 'dart:convert';
+ import 'package:url_launcher/url_launcher.dart';
+ import 'package:shared_preferences/shared_preferences.dart';
+
+ class Link {
+  String id;
+  String url;
+  String title;
+  String description;
+  String createdAt;
+
+  Link({this.id, this.url, this.title, this.description, this.createdAt});
+
+  Link.fromJson(Map<String, String> json)
+      : id = json['id'],
+        url = json['url'],
+        title = json['title'],
+        description = json['description'],
+        createdAt = json['createdAt'];
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'url': url,
+      'title': title,
+      'description': description,
+      'createdAt': createdAt,
+    };
+  }
+}
+    
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<Link> _items = [];
+
+  final myController = TextEditingController();
+
+  initState() {
+    super.initState();
+    init();
+  }
+
+  void init () async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    var list = prefs.getString('linkList');
+
+    setState(() {
+      var linkList = jsonDecode(list).map<Link>((item) {
+        return new Link(
+          id: item['id'],
+          url: item['url'],
+          title: item['title'],
+          description: item['description'],
+          createdAt: item['createdAt'],
+        );
+      }).toList();
+      _items = linkList;
+    });
+  }
+
+  getUrlData(final url) async {
+    var data = await MetadataFetch.extract(url);
+
+    return data;
+  }
+
+  void onReorder(int oldIndex, int newIndex) {
+    if (newIndex > oldIndex) {
+      newIndex -= 1;
+    }
+
+    setState(() {
+      Link link = _items[oldIndex];
+
+      _items.removeAt(oldIndex);
+      _items.insert(newIndex, link);
+    });
+  }
+
+  void addList () async {
+    String url = myController.text;
+    String id = DateFormat('yyyyMMddkkmmsss').format(DateTime.now());
+    String createdAt = DateFormat('yyyy-MM-dd').format(DateTime.now());
+    dynamic urlData = await getUrlData(url);
+
+    var result = Link(
+      id: id,
+      url: myController.text,
+      title: urlData.title != null ? urlData.title : '',
+      description: urlData.description != null ? urlData.description : '',
+      createdAt: createdAt,
+    );
+
+    setState(() {
+      _items.add(result);
+    });
+
+    setListAtLocal();
+  }
+
+  void setListAtLocal () async {
+    var list = jsonEncode(_items);
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('linkList', list);
+  }
+
+  List<Widget> _getListItems() => _items
+      .asMap()
+      .map((i, item) => MapEntry(i, _buildTenableListTile(item, i)))
+      .values
+      .toList();
+
+  Widget _buildTenableListTile(Link item, int index) {
+    return Dismissible(
+      key: Key(item.id),
+      dismissThresholds: {
+        DismissDirection.endToStart: 0.4,
+        DismissDirection.startToEnd: 0.4,
+      },
+      crossAxisEndOffset: 0.1,
+      onDismissed: (direction) {
+        setState(() {
+          _items.removeAt(index);
+        });
+        setListAtLocal();
+      },
+      background: Container(color: Colors.red),
+      child: ListTile(
+        key: ValueKey(item.id),
+        title: Row(
+          children: <Widget>[
+            Expanded(
+              flex: 2,
+              child: Text(
+                "${item.title}",
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            Expanded(
+              flex: 5,
+              child: Container(
+              margin: const EdgeInsets.only(left: 10.0),
+                child : Text(
+                  "${item.description}",
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Container(
+              margin: const EdgeInsets.only(left: 10.0),
+                child : Text("${item.createdAt}")
+              ),
+            ),
+          ],
+        ),
+        subtitle: Text(
+          '${item.url}',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        onTap: () async => await launch('${item.url}'),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ReorderableListView(
+        onReorder: onReorder,
+        children: _getListItems(),
+      ),
+      bottomSheet: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: TextField(
+                controller: myController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'url',
+                )
+              ),
+            ),
+            Container(
+              width: 80.0,
+              height: 60.0,
+              margin: const EdgeInsets.only(left: 10.0),
+              child : ElevatedButton(
+                onPressed: addList,
+                child: Text(
+                  'add',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ),
+          ],
+        )
+      )
+    );
+  }
+}
